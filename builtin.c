0a93c61cd2546869df1404e982dff63ba7744fc8
Rhnkey24 <lawalronke24@gmail.com>
2022-12-22 09:07:33 -0800

    This are additional files to the collaboration.

--- builtin.c
+++ builtin.c
@@ -10,12 +10,8 @@
  */
 
 int _myexit(info_t *info)
-
 {
-
 	int exitcheck;
-
-
 
 	if (info->argv[1])  /* If there is an exit arguement */
 
@@ -24,31 +20,19 @@ int _myexit(info_t *info)
 		exitcheck = _erratoi(info->argv[1]);
 
 		if (exitcheck == -1)
-
 		{
-
 			info->status = 2;
-
 			print_error(info, "Illegal number: ");
-
 			_eputs(info->argv[1]);
-
 			_eputchar('\n');
-
 			return (1);
-
 		}
-
 		info->err_num = _erratoi(info->argv[1]);
-
 		return (-2);
-
 	}
-
 	info->err_num = -1;
 
 	return (-2);
-
 }
 
 /**
@@ -60,16 +44,12 @@ int _myexit(info_t *info)
  */
 
 int _mycd(info_t *info)
-
 {
-
 	char *s, *dir, buffer[1024];
-
 	int chdir_ret;
 
 	s = getcwd(buffer, 1024);
 	if (!s)
-
 		_puts("TODO: >>getcwd failure emsg here<<\n");
 
 	if (!info->argv[1])
@@ -78,7 +58,6 @@ int _mycd(info_t *info)
 		if (!dir)
 		chdir_ret = /* TODO: what should this be? */
 		chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
-
 		else
 			chdir_ret = chdir(dir);
 	}
@@ -88,21 +67,19 @@ int _mycd(info_t *info)
 		{
 			_puts(s);
 			_putchar('\n');
-
 			return (1);
 		}
 		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
-		chdir_ret = /* TODO: what should this be? */
-		chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+		chdir_ret = chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
 	}
 	else
 		chdir_ret = chdir(info->argv[1]);
 	if (chdir_ret == -1)
 	{
-		print_error(info, "can't cd to ");
-		_eputs(info->argv[1]), _eputchar('\n');
+	print_error(info, "can't cd to ");
+	_eputs(info->argv[1]), _eputchar('\n');
 	}
-else
+	else
 	{
 	_setenv(info, "OLDPWD", _getenv(info, "PWD="));
 	_setenv(info, "PWD", getcwd(buffer, 1024));
